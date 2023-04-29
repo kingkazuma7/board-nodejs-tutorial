28489061c469d3e9eb979341875122354ac91bec
kingkazuma7 <your.nasionasio7@gmail.com>
2023-04-29 12:56:31 +0900

    firabase install

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAiuoa5NCbTaD2O_e53XQkeTztQeRhSsjI",
+  authDomain: "mongo-thread.firebaseapp.com",
+  projectId: "mongo-thread",
+  storageBucket: "mongo-thread.appspot.com",
+  messagingSenderId: "1066568813726",
+  appId: "1:1066568813726:web:9dd8154e51bc44b6713f8f"
+}
+
+const app = initializeApp(firebaseConfig);
